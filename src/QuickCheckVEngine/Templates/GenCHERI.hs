e770fdd573d9df497a7b16ec9b6a14239458360e
Peter Rugg <pdr32@cam.ac.uk>
2020-06-02 12:42:45 +0100

    Exclude cap sq since its non-determinism poses problems

--- src/QuickCheckVEngine/Templates/GenCHERI.hs
+++ src/QuickCheckVEngine/Templates/GenCHERI.hs
@@ -52,7 +52,10 @@ genRandomCHERITest arch = do
   tmpReg2   <- src
   dest      <- dest
   imm       <- bits 12
-  mop       <- bits 5
+  mop       <- elements [ 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
+                          0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf,
+                          -- Skip LR,SC since these have non-determinism which is problematic for TestRIG
+                          0x17,0x1f]
   longImm   <- (bits 20)
   fenceOp1  <- (bits 4)
   fenceOp2  <- (bits 4)
