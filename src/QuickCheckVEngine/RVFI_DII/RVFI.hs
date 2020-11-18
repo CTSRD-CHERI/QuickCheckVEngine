b9fd111fd15afd29c45851e9c056547bbb36a37b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-11-18 13:25:03 +0000

    Prepare the RVFI module for version 2 traces

--- src/QuickCheckVEngine/RVFI_DII/RVFI.hs
+++ src/QuickCheckVEngine/RVFI_DII/RVFI.hs
@@ -4,6 +4,7 @@
 -- Copyright (c) 2018 Matthew Naylor
 -- Copyright (c) 2018 Jonathan Woodruff
 -- Copyright (c) 2018-2020 Alexandre Joannou
+-- Copyright (c) 2020 Alex Richardson
 -- All rights reserved.
 --
 -- This software was developed by SRI International and the University of
@@ -37,34 +38,32 @@
 -- SUCH DAMAGE.
 --
 
-{-|
-    Module      : QuickCheckVEngine.RVFI_DII.RVFI
-    Description : RISC-V Formal Interface
-
-    The 'QuickCheckVEngine.RVFI_DII.RVFI' module defines the RISC-V RVFI
-    interface as introduced
-    <https://github.com/SymbioticEDA/riscv-formal/blob/master/docs/rvfi.md here>
--}
-
-module QuickCheckVEngine.RVFI_DII.RVFI (
-  RVFI_Packet (..)
-, rvfiEmptyHaltPacket
-, rvfiGetFromString
-, rvfiIsHalt
-, rvfiHaltVersion
-, rvfiIsTrap
-, rvfiCheck
-, rvfiCheckAndShow
-) where
+-- |
+--    Module      : QuickCheckVEngine.RVFI_DII.RVFI
+--    Description : RISC-V Formal Interface
+--
+--    The 'QuickCheckVEngine.RVFI_DII.RVFI' module defines the RISC-V RVFI
+--    interface as introduced
+--    <https://github.com/SymbioticEDA/riscv-formal/blob/master/docs/rvfi.md here>
+module QuickCheckVEngine.RVFI_DII.RVFI
+  ( RVFI_Packet,
+    rvfiEmptyHaltPacket,
+    rvfiGetFromString,
+    rvfiIsHalt,
+    rvfiHaltVersion,
+    rvfiIsTrap,
+    rvfiCheck,
+    rvfiCheckAndShow,
+  )
+where
 
 import Data.Word
+import Data.Binary
+import Data.Binary.Get
 import Data.Bits
 import qualified Data.Bits.Bitwise as BW
-import Data.Binary
-import Data.Binary.Put
-import Data.Binary.Get
 import qualified Data.ByteString.Lazy as BS
-import Data.Semigroup -- Should no longer be required with modern ghc
+import Data.Maybe
 import RISCV
 import Text.Printf
 
@@ -74,46 +73,38 @@ type RV_RegIdx = Word8
 -- | Type synonym for a RISCV XLEN-bit word
 type RV_WordXLEN = Word64
 
--- | Type synonym for a RISCV XLEN-bit word
-type RV_WordXLEN_ByteMask = Word8
+data RV_PrivMode = PRV_U | PRV_S | PRV_Reserved | PRV_M deriving (Enum, Show)
+
+type RV_XL = Word8
 
 -- * Definition of the RISC-V Formal Interface
 
 -- | The 'RVFI_Packet' type captures (a subset of) the RISC-V Formal Interface
-data RVFI_Packet = RVFI_Packet {
--- Metadata
-  rvfi_valid :: Word8
-, rvfi_order :: Word64
-, rvfi_insn  :: Word64
-, rvfi_trap  :: Word8
-, rvfi_halt  :: Word8
-, rvfi_intr  :: Word8
---, rvfi_mode  :: RV_PrivMode -- TODO
---, rvfi_ixl   :: RV_XL       -- TODO
--- Integer Register Read/Write
-, rvfi_rs1_addr  :: RV_RegIdx
-, rvfi_rs2_addr  :: RV_RegIdx
-, rvfi_rs1_rdata :: RV_WordXLEN
-, rvfi_rs2_rdata :: RV_WordXLEN
-, rvfi_rd_addr   :: RV_RegIdx
-, rvfi_rd_wdata  :: RV_WordXLEN
--- Program Counter
-, rvfi_pc_rdata :: RV_WordXLEN
-, rvfi_pc_wdata :: RV_WordXLEN
--- Memory Access
-, rvfi_mem_addr  :: RV_WordXLEN
-, rvfi_mem_rmask :: RV_WordXLEN_ByteMask
-, rvfi_mem_wmask :: RV_WordXLEN_ByteMask
-, rvfi_mem_rdata :: RV_WordXLEN
-, rvfi_mem_wdata :: RV_WordXLEN
--- Further TODOs in the RVFI specification:
--- Control and Status Registers (CSRs)
--- Modelling of Floating-Point State
--- Handling of Speculative Execution
--- Modelling of Virtual Memory
--- Modelling of Atomic Memory Operations
--- Skipping instructions
-}
+data RVFI_Packet = RVFI_Packet
+  { -- Metadata
+    rvfi_valid :: Word8,
+    rvfi_order :: Word64,
+    rvfi_insn :: Word64,
+    rvfi_trap :: Word8,
+    rvfi_halt :: Word8,
+    rvfi_intr :: Word8,
+    rvfi_mode :: Maybe RV_PrivMode,
+    rvfi_ixl :: Maybe RV_XL,
+    -- Program Counter
+    rvfi_pc_rdata :: RV_WordXLEN,
+    rvfi_pc_wdata :: RV_WordXLEN,
+    -- Integer Register Read/Write
+    rvfi_int_data :: Maybe RVFI_IntData,
+    -- Memory Access
+    rvfi_mem_data :: Maybe RVFI_MemAccessData
+    -- Further TODOs in the RVFI specification:
+    -- Control and Status Registers (CSRs)
+    -- Modelling of Floating-Point State
+    -- Handling of Speculative Execution
+    -- Modelling of Virtual Memory
+    -- Modelling of Atomic Memory Operations
+    -- Skipping instructions
+  }
 
 rvfiGetFromString "valid"     = Just $ toInteger . rvfi_valid
 rvfiGetFromString "order"     = Just $ toInteger . rvfi_order
@@ -136,66 +127,97 @@ rvfiGetFromString "mem_rdata" = Just $ toInteger . rvfi_mem_rdata
 rvfiGetFromString "mem_wdata" = Just $ toInteger . rvfi_mem_wdata
 rvfiGetFromString _           = Nothing
 
+data RVFI_IntData = RVFI_IntData
+  { rvfi_rs1_addr :: RV_RegIdx,
+    rvfi_rs2_addr :: RV_RegIdx,
+    rvfi_rs1_rdata :: RV_WordXLEN,
+    rvfi_rs2_rdata :: RV_WordXLEN,
+    rvfi_rd_addr :: RV_RegIdx,
+    rvfi_rd_wdata :: RV_WordXLEN
+  }
+
+rvfiEmptyIntData :: RVFI_IntData
+rvfiEmptyIntData =
+  RVFI_IntData
+    { rvfi_rs1_addr = 0,
+      rvfi_rs2_addr = 0,
+      rvfi_rs1_rdata = 0,
+      rvfi_rs2_rdata = 0,
+      rvfi_rd_addr = 0,
+      rvfi_rd_wdata = 0
+    }
+
+data RVFI_MemAccessData = RVFI_MemAccessData
+  { rvfi_mem_addr :: RV_WordXLEN,
+    rvfi_mem_rmask :: Word32,
+    rvfi_mem_wmask :: Word32,
+    rvfi_mem_rdata :: RV_WordXLEN,
+    rvfi_mem_wdata :: RV_WordXLEN
+  }
+
+rvfiEmptyMemData :: RVFI_MemAccessData
+rvfiEmptyMemData =
+  RVFI_MemAccessData
+    { rvfi_mem_addr = 0,
+      rvfi_mem_rmask = 0,
+      rvfi_mem_wmask = 0,
+      rvfi_mem_rdata = 0,
+      rvfi_mem_wdata = 0
+    }
+
 instance Binary RVFI_Packet where
-  put pkt =    putWord8    (rvfi_intr pkt)
-            <> putWord8    (rvfi_halt pkt)
-            <> putWord8    (rvfi_trap pkt)
-            <> putWord8    (rvfi_rd_addr pkt)
-            <> putWord8    (rvfi_rs2_addr pkt)
-            <> putWord8    (rvfi_rs1_addr pkt)
-            <> putWord8    (rvfi_mem_wmask pkt)
-            <> putWord8    (rvfi_mem_rmask pkt)
-            <> putWord64be (rvfi_mem_wdata pkt)
-            <> putWord64be (rvfi_mem_rdata pkt)
-            <> putWord64be (rvfi_mem_addr pkt)
-            <> putWord64be (rvfi_rd_wdata pkt)
-            <> putWord64be (rvfi_rs2_rdata pkt)
-            <> putWord64be (rvfi_rs1_rdata pkt)
-            <> putWord64be (rvfi_insn pkt)
-            <> putWord64be (rvfi_pc_wdata pkt)
-            <> putWord64be (rvfi_pc_rdata pkt)
-            <> putWord64be (rvfi_order pkt)
-  get = do intr      <- getWord8
-           halt      <- getWord8
-           trap      <- getWord8
-           rd_addr   <- getWord8
-           rs2_addr  <- getWord8
-           rs1_addr  <- getWord8
-           mem_wmask <- getWord8
-           mem_rmask <- getWord8
-           mem_wdata <- getWord64be
-           mem_rdata <- getWord64be
-           mem_addr  <- getWord64be
-           rd_wdata  <- getWord64be
-           rs2_rdata <- getWord64be
-           rs1_rdata <- getWord64be
-           insn      <- getWord64be
-           pc_wdata  <- getWord64be
-           pc_rdata  <- getWord64be
-           order     <- getWord64be
-           return $ RVFI_Packet {
-               rvfi_valid = 1
-             , rvfi_order = order
-             , rvfi_insn  = insn
-             , rvfi_trap  = trap
-             , rvfi_halt  = halt
-             , rvfi_intr  = intr
-             --, rvfi_mode = _
-             --, rvfi_ixl  = _
-             , rvfi_rs1_addr  = rs1_addr
-             , rvfi_rs2_addr  = rs2_addr
-             , rvfi_rs1_rdata = rs1_rdata
-             , rvfi_rs2_rdata = rs2_rdata
-             , rvfi_rd_addr   = rd_addr
-             , rvfi_rd_wdata  = rd_wdata
-             , rvfi_pc_rdata  = pc_rdata
-             , rvfi_pc_wdata  = pc_wdata
-             , rvfi_mem_addr  = mem_addr
-             , rvfi_mem_rmask = mem_rmask
-             , rvfi_mem_wmask = mem_wmask
-             , rvfi_mem_rdata = mem_rdata
-             , rvfi_mem_wdata = mem_wdata
-             }
+  put _ = error "Should not be called, we only read packets"
+  get = do
+    intr <- getWord8
+    halt <- getWord8
+    trap <- getWord8
+    rd_addr <- getWord8
+    rs2_addr <- getWord8
+    rs1_addr <- getWord8
+    mem_wmask <- getWord8
+    mem_rmask <- getWord8
+    mem_wdata <- getWord64be
+    mem_rdata <- getWord64be
+    mem_addr <- getWord64be
+    rd_wdata <- getWord64be
+    rs2_rdata <- getWord64be
+    rs1_rdata <- getWord64be
+    insn <- getWord64be
+    pc_wdata <- getWord64be
+    pc_rdata <- getWord64be
+    order <- getWord64be
+    return $
+      RVFI_Packet
+        { rvfi_valid = 1,
+          rvfi_order = order,
+          rvfi_insn = insn,
+          rvfi_trap = trap,
+          rvfi_halt = halt,
+          rvfi_intr = intr,
+          rvfi_mode = Nothing,
+          rvfi_ixl = Nothing,
+          rvfi_pc_rdata = pc_rdata,
+          rvfi_pc_wdata = pc_wdata,
+          rvfi_int_data =
+            Just
+              RVFI_IntData
+                { rvfi_rs1_addr = rs1_addr,
+                  rvfi_rs2_addr = rs2_addr,
+                  rvfi_rs1_rdata = rs1_rdata,
+                  rvfi_rs2_rdata = rs2_rdata,
+                  rvfi_rd_addr = rd_addr,
+                  rvfi_rd_wdata = rd_wdata
+                },
+          rvfi_mem_data =
+            Just
+              RVFI_MemAccessData
+                { rvfi_mem_addr = mem_addr,
+                  rvfi_mem_rmask = fromIntegral mem_rmask, -- FIXME: sign-extend/zero-extend?
+                  rvfi_mem_wmask = fromIntegral mem_wmask, -- FIXME: sign-extend/zero-extend?
+                  rvfi_mem_rdata = mem_rdata,
+                  rvfi_mem_wdata = mem_wdata
+                }
+        }
 
 -- | An otherwise empty halt token for padding
 rvfiEmptyHaltPacket :: RVFI_Packet
@@ -204,15 +226,22 @@ rvfiEmptyHaltPacket = (runGet get (BS.repeat 0)) { rvfi_halt = 1 }
 instance Show RVFI_Packet where
   show tok
     | rvfiIsHalt tok = printf "halt token v%d" (rvfiHaltVersion tok)
-    | otherwise = printf "Trap: %5s, PCWD: 0x%016x, RD: %02d, RWD: 0x%016x, MA: 0x%016x, MWD: 0x%016x, MWM: 0b%08b, I: 0x%016x (%s)"
-                  (show $ rvfi_trap tok /= 0) -- Trap
-                  (rvfi_pc_wdata tok)    -- PCWD
-                  (rvfi_rd_addr tok)     -- RD
-                  (rvfi_rd_wdata tok)    -- RWD
-                  (rvfi_mem_addr tok)    -- MA
-                  (rvfi_mem_wdata tok)   -- MWD
-                  (rvfi_mem_wmask tok)   -- MWM
-                  (rvfi_insn tok) (pretty (toInteger (rvfi_insn tok))) -- Inst
+    | otherwise =
+      printf
+        "Trap: %5s, PCWD: 0x%016x, RD: %02d, RWD: 0x%016x, MA: 0x%016x, MWD: 0x%016x, MWM: 0b%08b, I: 0x%016x P:%s (%s)"
+        (show $ rvfi_trap tok /= 0) -- Trap
+        (rvfi_pc_wdata tok) -- PCWD
+        (rvfi_rd_addr intData) -- RD
+        (rvfi_rd_wdata intData) -- RWD
+        (rvfi_mem_addr memData) -- MA
+        (rvfi_mem_wdata memData) -- MWD
+        (rvfi_mem_wmask memData) -- MWM
+        (rvfi_insn tok)
+        (show (rvfi_mode tok))
+        (pretty (toInteger (rvfi_insn tok))) -- Inst
+    where
+      intData = fromMaybe rvfiEmptyIntData (rvfi_int_data tok)
+      memData = fromMaybe rvfiEmptyMemData (rvfi_mem_data tok)
 
 -- | Return 'True' for halt 'RVFI_Packet's
 rvfiIsHalt :: RVFI_Packet -> Bool
@@ -227,26 +256,34 @@ rvfiIsTrap :: RVFI_Packet -> Bool
 rvfiIsTrap x = rvfi_trap x /= 0
 
 -- | Compare 'RVFI_Packet's
+-- TODO: Improve handling of Maybe values
 rvfiCheck :: Bool -> RVFI_Packet -> RVFI_Packet -> Bool
 rvfiCheck is64 x y
-  | rvfiIsHalt x = (rvfi_halt x) == (rvfi_halt y)
-  | rvfiIsTrap x = ((rvfi_trap x) == (rvfi_trap y)) && (maskUpper is64 (rvfi_pc_wdata x)) == (maskUpper is64 (rvfi_pc_wdata y))
-  | otherwise = (maskUpper False $ rvfi_insn x) == (maskUpper False $ rvfi_insn y) &&
-                (rvfi_trap x) == (rvfi_trap y) && (rvfi_halt x) == (rvfi_halt y) &&
-                (rvfi_rd_addr x == rvfi_rd_addr y) &&
-                ((rvfi_rd_addr x == 0) || (maskUpper is64 (rvfi_rd_wdata x) == maskUpper is64 (rvfi_rd_wdata y))) &&
-                (rvfi_mem_wmask x) == (rvfi_mem_wmask y) &&
-                ((rvfi_mem_wmask x == 0) || ((maskUpper is64 (rvfi_mem_addr x)) == (maskUpper is64 (rvfi_mem_addr y)))) &&
-                (maskUpper is64 (rvfi_pc_wdata x)) == (maskUpper is64 (rvfi_pc_wdata y)) &&
-                (maskWith (rvfi_mem_wdata x) (rvfi_mem_wmask x)) == (maskWith (rvfi_mem_wdata y) (rvfi_mem_wmask y))
-  where maskUpper is64 x = if is64 then x else (x Data.Bits..&. 0x00000000FFFFFFFF)
-        byteMask2bitMask mask = BW.fromListLE $ concatMap ((take 8).repeat) (BW.toListLE mask)
-        maskWith a b = a Data.Bits..&. byteMask2bitMask b
+  | rvfiIsHalt x = rvfi_halt x == rvfi_halt y
+  | rvfiIsTrap x = (rvfi_trap x == rvfi_trap y) && (maskUpper is64 (rvfi_pc_wdata x) == maskUpper is64 (rvfi_pc_wdata y))
+  | otherwise =
+    (maskUpper False (rvfi_insn x) == maskUpper False (rvfi_insn y))
+      && (rvfi_trap x == rvfi_trap y)
+      && (rvfi_halt x == rvfi_halt y)
+      && (rvfi_rd_addr xInt == rvfi_rd_addr yInt)
+      && ((rvfi_rd_addr xInt == 0) || (maskUpper is64 (rvfi_rd_wdata xInt) == maskUpper is64 (rvfi_rd_wdata yInt)))
+      && (rvfi_mem_wmask xMem == rvfi_mem_wmask yMem)
+      && ((rvfi_mem_wmask xMem == 0) || (maskUpper is64 (rvfi_mem_addr xMem) == maskUpper is64 (rvfi_mem_addr yMem)))
+      && (maskUpper is64 (rvfi_pc_wdata x) == maskUpper is64 (rvfi_pc_wdata y))
+      && (maskWith (rvfi_mem_wdata xMem) (rvfi_mem_wmask xMem) == maskWith (rvfi_mem_wdata yMem) (rvfi_mem_wmask yMem))
+  where
+    maskUpper _is64 _x = if _is64 then _x else _x Data.Bits..&. 0x00000000FFFFFFFF
+    byteMask2bitMask mask = BW.fromListLE $ concatMap (replicate 8) (BW.toListLE mask)
+    maskWith a b = a Data.Bits..&. byteMask2bitMask b
+    xInt = fromMaybe rvfiEmptyIntData (rvfi_int_data x)
+    yInt = fromMaybe rvfiEmptyIntData (rvfi_int_data y)
+    xMem = fromMaybe rvfiEmptyMemData (rvfi_mem_data x)
+    yMem = fromMaybe rvfiEmptyMemData (rvfi_mem_data y)
 
 -- | Compare 2 'RVFI_Packet's and produce a 'String' output displaying the
 --   the content of the packet once only for equal inputs or the content of
 --   each input 'RVFI_Packet' if inputs are not succeeding the 'rvfiCheck'
 rvfiCheckAndShow :: Bool -> RVFI_Packet -> RVFI_Packet -> (Bool, String)
 rvfiCheckAndShow is64 x y
-  | rvfiCheck is64 x y = (True,  "     " ++ show x)
-  | otherwise          = (False, " A < " ++ show x ++ "\n B > " ++ show y)
+  | rvfiCheck is64 x y = (True, "     " ++ show x)
+  | otherwise = (False, " A < " ++ show x ++ "\n B > " ++ show y)
