a7463966551335f7f274f28403101b7a87c29b86
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-11-22 13:14:52 +0000

    Fix some unused warnings

--- src/QuickCheckVEngine/Main.hs
+++ src/QuickCheckVEngine/Main.hs
@@ -211,7 +211,7 @@ main = withSocketsDo $ do
         when (optVerbosity flags > 0) $
           do putStrLn "Replaying shrunk failed test case:"
              let tcNew = tcTrans tc traceA traceB
-             checkSingle tcNew 2 False (testLen flags) (const $ return ())
+             _ <- checkSingle tcNew 2 False (testLen flags) (const $ return ())
              return ()
         when (optSave flags) $
           do case (saveDir flags) of
