98505400feea000a7b0b0b56c0495e32ece16dcc
Franz Fuchs <franz.fuchs@cl.cam.ac.uk>
2021-06-14 13:58:22 +0100

    Added first attempts for verifying SBC jumps

--- src/QuickCheckVEngine/Main.hs
+++ src/QuickCheckVEngine/Main.hs
@@ -189,6 +189,7 @@ allTests = [
              ("arith",      "Arithmetic Verification",                                const True,                       const $ repeatTemplateTillEnd gen_rv32_i_arithmetic)
            , ("data_scc",   "Data-Speculative Capability Constraint Verifier",        andPs [has_cheri, has_icsr],      const gen_data_scc_verify)
            , ("sbc_cond_1", "Speculative Branching Constraint Condition 1 Verifier",  andPs [has_cheri, has_icsr],      const gen_sbc_cond_1_verify)
+           , ("sbc_jumps",  "Speculative Branching Constraint Jumps Verifier",        andPs [has_cheri, has_icsr],      const gen_sbc_jumps_verify)
            , ("mem",        "Memory Verification",                                    const True,                       const $ repeatTemplateTillEnd gen_rv32_i_memory)
            , ("control",    "Control Flow Verification",                              const True,                       const $ repeatTemplateTillEnd gen_rv32_i_controlflow)
            , ("cache",      "Cache Verification",                                     const True,                       const $ repeatTemplateTillEnd gen_rv32_i_cache)
