2b11937d876c8fc165786eb5c8a7297f2c146729
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2026-07-27 16:56:57 +0100

    Include xcheri A tests in caprandom
    With this, we seem to test all the relevant capability amo instructions.
    It seems we only fail due to genuine non-determinism: most notably
    SCs on CVA6 fail if the LR pulled the line into the cache

--- src/QuickCheckVEngine/Templates/GenCHERI.hs
+++ src/QuickCheckVEngine/Templates/GenCHERI.hs
@@ -112,6 +112,7 @@ genRandomCHERITest = readParams $ \param -> random $ do
                 , (10, instUniform $ rv32_i srcAddr srcData dest imm longImm fenceOp1 fenceOp2)
                 , (10, instUniform $ rv64_a srcAddr srcData dest 0 0)
                 , (10, instUniform $ rv32_a srcAddr srcData dest 0 0)
+                , (10, instUniform $ rv32_a_xcheri srcAddr srcData dest 0 0)
                 , (10, instUniform $ rv32_xcheri arch srcAddr srcData imm dest)
                 , (5, instUniform $ rv32_zicsr srcData dest csrAddr uimm5)
                 , (20, switchEncodingMode)
