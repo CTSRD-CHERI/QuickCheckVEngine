0f93c95841c2ad060c4b6dee254924e0645b9731
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2026-09-04 17:11:45 +0100

    Update PTE test for 0.9.9 PTE encoding

--- src/QuickCheckVEngine/Templates/GenMemory.hs
+++ src/QuickCheckVEngine/Templates/GenMemory.hs
@@ -182,12 +182,12 @@ gen_pte_perms = random $
   do lperms_rng <- bits 10
      let lperms_all = 0x3ff
      lperms <- choose (lperms_all, lperms_rng)
-     uperms_legal <- bits 3
+     uperms <- bits 4
      let satp = unsafe_csrs_indexFromName "satp"
      let sstatus = unsafe_csrs_indexFromName "sstatus"
      let excCSRs = unsafe_csrs_indexFromName <$> ["mcause", "mtval"]
-     return $ shrinkScope $ instSeq [ori 1 0 uperms_legal, -- three cheri pte bits
-                                     slli 1 1 13,
+     return $ shrinkScope $ instSeq [ori 1 0 uperms, -- 4 cheri pte bits
+                                     slli 1 1 12,
                                      ori 1 1 0x000, -- 11 msbs of PA
                                      slli 1 1 11,
                                      ori 1 1 0x000, -- next 11 bits of PA
@@ -213,7 +213,11 @@ gen_pte_perms = random $
                                      <>
                                      uniform [csrs sstatus 8, csrc sstatus 8]
                                      <>
-                                     li64 8 0x1000000000000000 -- crge
+                                     li64 8 0x1000000000000000 -- ycrg
+                                     <>
+                                     uniform [csrs sstatus 8, csrc sstatus 8]
+                                     <>
+                                     li64 8 0x0800000000000000 -- yrge
                                      <>
                                      uniform [csrs sstatus 8, csrc sstatus 8]
                                      <>
